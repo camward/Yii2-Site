06a876cba7fd533083451cf98016c779e0b286db
alex <camward@yandex.ru>
2016-12-22 12:30:20 +0400

    start project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-site',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
