7e5b86ee9cba8020fd52e6dc62c07b88f10e543d
alex <camward@yandex.ru>
2016-12-22 13:58:45 +0400

    подключение шаблона

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,12 +18,27 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/reset.css',
+        'css/style.css',
+        'css/grid.css',
+        'css/prettyPhoto.css',
     ];
     public $js = [
+        'js/jquery-1.6.3.min.js',
+        'js/cufon-yui.js',
+        'js/cufon-replace.js',
+        'js/Vegur_700.font.js',
+        'js/Vegur_400.font.js',
+        'js/FF-cash.js',
+        'js/script.js',
+        'js/easyTooltip.js',
+        'js/jquery.easing.1.3.js',
+        'js/hover-image.js',
+        'js/jquery.prettyPhoto.js',
+        'js/tms-0.3.js',
+        'js/tms_presets.js',
+        'js/index.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
     ];
 }
--- config/web.php
+++ config/web.php
@@ -6,6 +6,9 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'layout' => 'one-template',
+    'language' => 'ru',
+    'charset' => 'utf-8',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
